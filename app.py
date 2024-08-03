056b4934b9bf4dcbe687cc68793700bc6704bd46
felix <famaldonadoos@gmail.com>
2024-08-02 22:20:41 -0500

    added main.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from getlinks import GetLinks
 import subprocess
 from downloader import Downloader
-from asciiart import AsciiArt
+
 """
 This program keeps track of all of our books
 version : 2.0.0
@@ -109,60 +108,3 @@ class App:
         for index, (url, _) in enumerate(urls, start=1):
             print(f"{index}) {url}")
         print(f"{len(urls) + 1}) Quit")
-
-def intro(header="PyZiggy"):
-    # AsciiArt(header, font="tinker-toy").display()
-    print('')
-    AsciiArt(header, font="basic").display()
-    print("\nWelcome to the Zig downloader!")
-    print("This program will help you download Zig for your system.")
-    print("Let's get started!\n")
-    print("Author: Alexander Maldonado (felixmaldonado2023@u.northwestern.edu)")
-    print("Date: 08-02-2024")
-
-if __name__=='__main__':
-    
-    # start app 
-    app = App()
-    app.intro = intro
-    
-    # run
-    choices = app.run_selector()
-    url = 'https://ziglang.org/download/'
-    gl = GetLinks(url)
-    gl.run()
-    
-    my_urls = None
-    if choices:
-        my_urls = gl.get_download_url(url_list=None, 
-                        os=choices['os'], 
-                        architecture=choices['architecture'], 
-                        has_signature=choices['has_signature'], 
-                        latest_version=choices['latest_version'])
-    
-    # check if urls are found and exit if not    
-    if not my_urls or len(my_urls) == 0: 
-        print("No urls found, exiting...")
-        exit()
-
-    # if found: display urls to download 
-    while True:
-        app.display_url_menu(my_urls)
-        try:
-            choice = int(input("Enter your choice: "))
-        except ValueError:
-            print("Invalid input, please enter a number.")
-            continue
-
-        if 1 <= choice <= len(my_urls):
-            selected_url = my_urls[choice - 1][0]
-            if app.download_url(selected_url): print("Download successful!")
-            else: print("Download failed.")
-            break
-        elif choice == len(my_urls) + 1:
-            print("Quitting...")
-            break
-        else:
-            print("Invalid choice, please try again.")
-            
-    app.outro()
